0db6e6aefa4d2eb1b623aacc037dabea7b704466
well <well@corp.globo.com>
2019-07-20 12:13:47 -0300

    Finishing section-1

---
+++ Section-01/1.15-python_code_style.py
@@ -0,0 +1,17 @@
+def nome_da_funcao(parametro1,parametro2):
+    quatro_espacos na identacao
+
+
+Listas = ["se","forem","poucos","itens"]
+
+Listas = [
+       "se",
+       "forem",
+       "muitos",
+       "itens",
+       "colocar",
+       "por",
+       "linha"
+]
+
+if valor is None: #valor nulo
---
+++ Section-01/1.16-operadores.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+num1 = 5
+num2 = 4
+
+#Operadores matemáticos
+mult = num1 * num2
+adic = num1 + num2
+subt = num1 - num2
+divi = num1 / num2
+modu = num1 % num2
+
+print mult
+print adic
+print subt
+print divi
+print modu
+
+#Operadores racionais
+== igual
+!= ou <> diferente
+< menor
+> maior
+<= menor ou igual
+>= maior ou igual
+is boleano
+
+#Operadores lógicos
+and - var1 and var2 # é True se ambas forem verdadeiras
+or  - var1 or var2 # é True se alguma for verdadeira
+not - not var1 # é True se var1 for falso
+==  - var1 == var2 # é True se forem iguais
+!=  - var1 != var2 # é True se var1 for diferente de var2
